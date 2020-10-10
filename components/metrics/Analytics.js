f3f7cf41251f0909c8af8f0045843a570da30cc0
Michael Pierce <matchmike1313@gmail.com>
2020-10-10 14:34:45 -0400

    Change HOST_URL for analytics all time views on dashbaord

--- components/metrics/Analytics.js
+++ components/metrics/Analytics.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import useSWR from 'swr';
-import format from 'comma-number';
-
-import fetcher from '../../lib/fetcher';
-
-import MetricCard from './Card';
+import { HOST_URL } from "../../lib/constants";
+import MetricCard from "./Card";
+import React from "react";
+import fetcher from "../../lib/fetcher";
+import format from "comma-number";
+import useSWR from "swr";
 
 const Analytics = () => {
-  const { data } = useSWR('/api/page-views', fetcher);
+  const { data } = useSWR("/api/page-views", fetcher);
 
   const pageViews = format(data?.total);
-  const link = 'https://leerob.io';
 
-  return <MetricCard header="All-Time Views" link={link} metric={pageViews} />;
+  return (
+    <MetricCard header="All-Time Views" link={HOST_URL} metric={pageViews} />
+  );
 };
 
 export default Analytics;
